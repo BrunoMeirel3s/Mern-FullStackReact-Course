99fc7efedec1848c8020b3004b6148d155f5f9ef
Bruno Meireles <50882819+BrunoMeirel3s@users.noreply.github.com>
2020-11-03 06:04:15 -0300

    03-11-2020
    creation of the token validation process

--- DEVCONNECTOR/routes/api/auth.js
+++ DEVCONNECTOR/routes/api/auth.js
@@ -1,12 +1,23 @@
-const express = require('express')
-const router = express.Router()
-
+const express = require("express");
+const router = express.Router();
+const auth = require("../../middleware/auth"); //our method to verifie if the user has a valid token
+const User = require("../../models/User"); //The model that establish the connection with our database
 
 // @route  GET api/auth
 // @desc   Test Route
 // @access Public
-router.get('/', (req, res) =>{
-    res.send('Auth route')
-})
+/**
+ * Pay atention that we're passing auth as a parametrer to out router function, this way
+ * it will check if the user has a valid token before he access our route
+ */
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 
-module.exports = router
+module.exports = router;
