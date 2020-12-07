a49593d94f7611b91da9cbb6fa71a9b28af8cdbf
Bruno Meireles <50882819+BrunoMeirel3s@users.noreply.github.com>
2020-12-07 09:51:05 -0300

    07-12-2020

--- DEVCONNECTOR/client/src/actions/profile.js
+++ DEVCONNECTOR/client/src/actions/profile.js
@@ -33,37 +33,37 @@ export const getCurrentProfile = () => async (dispatch) => {
   }
 };
 
-//Create or update a profile
+// Create or update profile
+/**
+ * Pay attention that we're recei
+ */
 export const createProfile = (formData, history, edit = false) => async (
   dispatch
 ) => {
   try {
-    const config = {
-      headers: {
-        "Content-type": "application/json",
-      },
-    };
-
-    const res = await axios.post("/api/profile", formData, config);
+    const res = await axios.post('/api/profile', formData);
 
     dispatch({
       type: GET_PROFILE,
-      payload: res.data,
+      payload: res.data
     });
 
-    dispatch(setAlert(edit ? "Profile Updated" : "Profile Created", "success"));
+    dispatch(setAlert(edit ? 'Profile Updated' : 'Profile Created', 'success'));
 
     if (!edit) {
-      history.push("/dashboard");
+      history.push('/dashboard');
     }
   } catch (err) {
-    const errors = err.response.data.erros;
+    const errors = err.response.data.errors;
+
     if (errors) {
-      errors.forEach((error) => dispatch(setAlert(error.msg, "danger")));
+      errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
     }
+
     dispatch({
       type: PROFILE_ERROR,
-      payload: { msg: err.response.statusText, status: err.response.status },
+      payload: { msg: err.response.statusText, status: err.response.status }
     });
   }
 };
+
