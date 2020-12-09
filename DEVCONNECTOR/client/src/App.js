b9c052ecef778eea261900704e83c3bb8196affe
Bruno Meireles <50882819+BrunoMeirel3s@users.noreply.github.com>
2020-12-09 06:10:52 -0300

    09-12-2020
    Adding two new components

--- DEVCONNECTOR/client/src/App.js
+++ DEVCONNECTOR/client/src/App.js
@@ -7,6 +7,9 @@ import Login from "./components/auth/Login"; //Component that will be call in ou
 import Alert from "./components/layout/Alert";
 import DashBoard from "./components/dashboard/Dashboard";
 import CreateProfile from "./components/profile-form/CreateProfile";
+import EditProfile from "./components/profile-form/EditProfile";
+import AddExperience from "./components/profile-form/AddExperience";
+import AddEducation from "./components/profile-form/AddEducation";
 import PrivateRoute from "./components/routing/PrivateRoute";
 import "./App.css";
 //Redux
@@ -39,6 +42,21 @@ const App = () => {
                 exact
                 path="/create-profile"
                 component={CreateProfile}
+              />
+              <PrivateRoute
+                exact
+                path="/edit-profile"
+                component={EditProfile}
+              />
+              <PrivateRoute
+                exact
+                path="/add-experience"
+                component={AddExperience}
+              />
+              <PrivateRoute
+                exact
+                path="/add-education"
+                component={AddEducation}
               />
             </Switch>
           </section>
--- DEVCONNECTOR/client/src/components/profile-form/EditProfile.js
+++ DEVCONNECTOR/client/src/components/profile-form/EditProfile.js
@@ -1,11 +1,15 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import { Link, withRouter } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { createProfile } from "../../actions/profile";
+import { createProfile, getCurrentProfile } from "../../actions/profile";
 
-const CreateProfile = ({ createProfile, history }) => {
-  //values that we're going to use in our frontEnd inputs
+const EditProfile = ({
+  profile: { profile, loading },
+  createProfile,
+  getCurrentProfile,
+  history,
+}) => {
   const [formData, setFormData] = useState({
     company: "",
     website: "",
@@ -21,10 +25,34 @@ const CreateProfile = ({ createProfile, history }) => {
     instagram: "",
   });
 
-  //used to change the value of displaySocialInputs to true or false, this way we can hide or show the social inputs in frontEnd
   const [displaySocialInputs, toggleSocialInputs] = useState(false);
 
-  //here we're destructuring the values to be used in our inputs
+  /*
+   * useEffect here is used to load getCurrentProfile as soon as the component EditProfile be call
+   * this way getCurrentProfile() fills the profile state and we use the value of this state to fill
+   * the values of our inputs on frontEnd. '[loading]' means that we only use useEffect when loading be present
+   */
+
+  useEffect(() => {
+    getCurrentProfile();
+
+    setFormData({
+      company: loading || !profile.company ? "" : profile.company,
+      website: loading || !profile.website ? "" : profile.website,
+      location: loading || !profile.location ? "" : profile.location,
+      status: loading || !profile.status ? "" : profile.status,
+      skills: loading || !profile.skills ? "" : profile.skills.join(","),
+      githubusername:
+        loading || !profile.githubusername ? "" : profile.githubusername,
+      bio: loading || !profile.bio ? "" : profile.bio,
+      twitter: loading || !profile.social ? "" : profile.social.twitter,
+      facebook: loading || !profile.social ? "" : profile.social.facebook,
+      linkedin: loading || !profile.social ? "" : profile.social.linkedin,
+      youtube: loading || !profile.social ? "" : profile.social.youtube,
+      instagram: loading || !profile.social ? "" : profile.social.instagram,
+    });
+  }, [loading]);
+
   const {
     company,
     website,
@@ -40,13 +68,12 @@ const CreateProfile = ({ createProfile, history }) => {
     instagram,
   } = formData;
 
-  //onChange is used to set the values inserted in inputs to our values in formData
   const onChange = (e) =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
 
   const onSubmit = (e) => {
     e.preventDefault();
-    createProfile(formData, history);
+    createProfile(formData, history, true);
   };
   return (
     <Fragment>
@@ -224,8 +251,15 @@ const CreateProfile = ({ createProfile, history }) => {
   );
 };
 
-CreateProfile.propTypes = {
+EditProfile.propTypes = {
   createProfile: PropTypes.func.isRequired,
+  getCurrentProfile: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired,
 };
 
-export default connect(null, { createProfile })(withRouter(CreateProfile));
+const mapStateToProps = (state) => ({
+  profile: state.profile,
+});
+export default connect(mapStateToProps, { createProfile, getCurrentProfile })(
+  withRouter(EditProfile)
+);
